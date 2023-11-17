39920f2db9978ec299d525872ad6569482cb4f03
hannahmaschke <hannah.maschke@dcmail.ca>
2023-11-17 15:23:24 -0500

    declared alt text and began looping through images

--- lab4/part2/gallery-start/gallery-start/main.js
+++ lab4/part2/gallery-start/gallery-start/main.js
@@ -8,16 +8,27 @@ const overlay = document.querySelector('.overlay');
 const filenames = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
+pic1.alt = "Closeup image of a blue eye. "
+pic2.alt = "An interesting wave-like rock formation. "
+pic3.alt = "Purple flowers. "
+pic4.alt = "Egyptian hieroglyphs. "
+pic5.alt = "A brown moth on a green leaf. "
 
 /* Looping through images */
+for (const newImage of filenames) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', xxx);
+    newImage.setAttribute('alt', xxx);
+    thumbBar.appendChild(newImage);;
+
+  }
+
+  
+
 
 /* Add a click event listener to each <img> inside the thumb bar */
 
 
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */ /* Add a click event listener to the button */
