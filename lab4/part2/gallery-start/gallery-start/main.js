76f33d8d8a7d1ddc9933b19569df7a38ba61740c
hannahmaschke <hannah.maschke@dcmail.ca>
2023-11-17 16:12:28 -0500

    fixed the alt text

--- lab4/part2/gallery-start/gallery-start/main.js
+++ lab4/part2/gallery-start/gallery-start/main.js
@@ -8,11 +8,12 @@ const overlay = document.querySelector('.overlay');
 const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
-pic1.alt = "Closeup image of a blue eye. "
-pic2.alt = "An interesting wave-like rock formation. "
-pic3.alt = "Purple flowers. "
-pic4.alt = "Egyptian hieroglyphs. "
-pic5.alt = "A brown moth on a green leaf. "
+const alts = 
+{'pic1.jpg' : "Closeup image of a blue eye. ",
+'pic2.jpg' : "An interesting wave-like rock formation. ",
+'pic3.jpg' : "Purple flowers. ",
+'pic4.jpg' : "Egyptian hieroglyphs. ",
+'pic5.jpg' : "A brown moth on a green leaf. "};
 
 /* Looping through images */
 for (const image of images) {
