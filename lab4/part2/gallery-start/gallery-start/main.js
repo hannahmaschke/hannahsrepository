0e4458b287d07343b0849c29f4debf26254140d3
hannahmaschke <hannah.maschke@dcmail.ca>
2023-11-17 14:58:36 -0500

    declared array of filenames

--- lab4/part2/gallery-start/gallery-start/main.js
+++ lab4/part2/gallery-start/gallery-start/main.js
@@ -5,14 +5,19 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const filenames = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */
+
+/* Add a click event listener to each <img> inside the thumb bar */
+
+
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
 newImage.setAttribute('alt', xxx);
 thumbBar.appendChild(newImage);
 
-/* Wiring up the Darken/Lighten button */
+/* Wiring up the Darken/Lighten button */ /* Add a click event listener to the button */
