e1c47e12cdb72f62bf270edc85eb248f1ad1872d
hannahmaschke <hannah.maschke@dcmail.ca>
2023-11-17 15:53:10 -0500

    added event listener to each img, creating new event listener for the lighten/darken button

--- lab4/part2/gallery-start/gallery-start/main.js
+++ lab4/part2/gallery-start/gallery-start/main.js
@@ -5,7 +5,7 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const filenames = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
+const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
 pic1.alt = "Closeup image of a blue eye. "
@@ -15,20 +15,21 @@ pic4.alt = "Egyptian hieroglyphs. "
 pic5.alt = "A brown moth on a green leaf. "
 
 /* Looping through images */
-for (const newImage of filenames) {
-    const newImage = document.createElement('img');
-    newImage.setAttribute('src', xxx);
-    newImage.setAttribute('alt', xxx);
+for (const image of images) {
+    const image = document.createElement('img');
+    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute('alt', alts[image]);
     thumbBar.appendChild(newImage);;
-
-  }
-
-  
-
-
-/* Add a click event listener to each <img> inside the thumb bar */
-
-
-
+/* Add a click event listener to each <img> inside the thumb bar (this is supposed to be part of the loop) */
+  newImage.addEventlistener('click', e => 
+  {
+    displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
+  });
+}
 
 /* Wiring up the Darken/Lighten button */ /* Add a click event listener to the button */
+btn.addEventListener('click', e =>
+{
+    const btnclass = btnclass.getAttribute();
+})
