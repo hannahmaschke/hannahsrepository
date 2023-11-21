25d707dedafaa81fa41daae4d7979b2ea2348507
hannahmaschke <hannah.maschke@dcmail.ca>
2023-11-21 17:11:45 -0500

    Update main-finished.js
    replaced part of the code with if (ball.exists) to check if the ball exists

--- lab4/part4/main-finished.js
+++ lab4/part4/main-finished.js
@@ -87,7 +87,7 @@ class EvilCircle extends Shape  {
 
    collisionDetect() {
       for (const ball of balls) {
-        if (!(this === ball) && ball.exists) {
+        if (ball.exists) {
           const dx = this.x - ball.x;
           const dy = this.y - ball.y;
           const distance = Math.sqrt(dx * dx + dy * dy);
