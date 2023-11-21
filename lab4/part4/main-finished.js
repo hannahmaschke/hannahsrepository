2c7ec10d11aed42250c1cc15a1e4a47f2299c70f
hannahmaschke <hannah.maschke@dcmail.ca>
2023-11-21 17:32:18 -0500

    changed the loop function and added new evilcircle object

--- lab4/part4/main-finished.js
+++ lab4/part4/main-finished.js
@@ -173,14 +173,19 @@ while (balls.length < 25) {
   balls.push(ball);
 }
 
+const evilCircle2 = new evilCircle2(random(0, height), random(0, width))
+
 function loop() {
    ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
    ctx.fillRect(0, 0,  width, height);
 
-   for (const ball of balls) {
-     ball.draw();
-     ball.update();
-     ball.collisionDetect();
+    
+      for (const ball of balls){
+      if (ball.exists = true){
+      ball.draw();
+      ball.update();
+      ball.collisionDetect();
+      }
    }
 
    requestAnimationFrame(loop);
