292ff3e34347af30ba4e40bdd38939610df659b8
hannahmaschke <hannah.maschke@dcmail.ca>
2023-11-21 11:58:39 -0500

    added html for part 3

--- lab4/part2/gallery-start/gallery-start/main.js
+++ lab4/part2/gallery-start/gallery-start/main.js
@@ -9,16 +9,18 @@ const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
 const alts = 
-{'pic1.jpg' : 'Closeup image of a blue eye. ',
-'pic2.jpg' : 'An interesting wave-like rock formation. ',
-'pic3.jpg' : 'Purple flowers. ',
-'pic4.jpg' : 'Egyptian hieroglyphs. ',
-'pic5.jpg' : 'A brown moth on a green leaf. '}
+{
+    'pic1.jpg' : 'Closeup image of a blue eye. ',
+    'pic2.jpg' : 'An interesting wave-like rock formation. ',
+    'pic3.jpg' : 'Purple flowers. ',
+    'pic4.jpg' : 'Egyptian hieroglyphs. ',
+    'pic5.jpg' : 'A brown moth on a green leaf. '
+}
 
 /* Looping through images */
 for (const image of images) {
     const newImage = document.createElement('img');
-    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute('src', 'images/${image}');
     newImage.setAttribute('alt', alts[image]);
     thumbBar.appendChild(newImage);
 /* Add a click event listener to each <img> inside the thumb bar (this is supposed to be part of the loop) */
@@ -43,8 +45,6 @@ btn.addEventListener('click', ()  =>
     { 
         btn.setAttribute('class', 'dark');
         btn.textContent = 'Darken';
-        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
-
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';}
     }
-    
-});
+);
